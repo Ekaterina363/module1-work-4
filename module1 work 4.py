3a0007cac6c67122bda44b5ea913c7e7471f8933
Ekaterina363 <batsl363@gmail.com>
2024-11-14 21:14:28 +0400

    Add files via upload

---
+++ module1 work 4.py
@@ -0,0 +1,9 @@
+hom_work = 12
+print(hom_work)
+time = 1.5
+print(time)
+name_work = "Python"
+print(name_work)
+print(hom_work // time)
+print("Kurs:",  name_work,", vsego zadach:", hom_work,", zatracheno chasov:", time,", srednee vremy vipolneniy",  hom_work // time, "chasov.")
+
